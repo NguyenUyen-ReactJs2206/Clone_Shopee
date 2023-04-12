a585a87e15983edc8c5bd89b8ef54fbcf010400f
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-04-13 00:48:04 +0700

    Call Api for ProductList using React Query

--- ShopeeClone/src/apis/product.api.ts
+++ ShopeeClone/src/apis/product.api.ts
@@ -1,4 +1,14 @@
-// const URL = 'products'
-// const productApi = {
+import { Product, ProductList, ProductListConfig } from 'src/types/product.type'
+import { SuccessResponseApi } from 'src/types/utils.type'
+import http from 'src/utils/http'
 
-// }
+const URL = 'products'
+const productApi = {
+  getProducts: (params: ProductListConfig) =>
+    http.get<SuccessResponseApi<ProductList>>(URL, {
+      params
+    }),
+  getProductDetail: (id: string) => http.get<SuccessResponseApi<Product>>(`${URL}/${id}`)
+}
+
+export default productApi
