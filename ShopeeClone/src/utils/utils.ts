990003e5a554dfaebdd557885735fcf4a7d57ac5
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-04-21 00:01:02 +0700

    Handle URL Friendly SEO

--- ShopeeClone/src/utils/utils.ts
+++ ShopeeClone/src/utils/utils.ts
@@ -24,3 +24,17 @@ export function formatNumberToSocialStyle(value: number) {
 }
 
 export const rateSale = (original: number, sale: number) => Math.round(((original - sale) / original) * 100) + '%'
+
+const removeSpecialCharacter = (str: string) =>
+  // eslint-disable-next-line no-useless-escape
+  str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, '')
+export const generateNameId = ({ name, id }: { name: string; id: string }) => {
+  //replace(/\s/g, '-') -- chuyen dau cach thanh dau -
+  return removeSpecialCharacter(name).replace(/\s/g, '-') + `-i-${id}`
+}
+
+export const getIdFromNameId = (nameId: string) => {
+  const arr = nameId.split('-i-')
+  //lay item cuoi cung cua arr
+  return arr[arr.length - 1]
+}
