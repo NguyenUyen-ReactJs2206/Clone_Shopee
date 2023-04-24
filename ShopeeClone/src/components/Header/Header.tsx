8afa77a1ea9b04a27db65ccb49988116727b7a08
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-04-25 01:17:56 +0700

    Remove Purchase When Logout

--- ShopeeClone/src/components/Header/Header.tsx
+++ ShopeeClone/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { QueryClient, useMutation, useQuery } from '@tanstack/react-query'
 import { Link, createSearchParams, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import Popover from '../Popover'
@@ -14,6 +14,7 @@ import { purchasesStatus } from 'src/constants/purchase'
 import purchaseApi from 'src/apis/purchase.api'
 import noproduct from '../../assets/images/no-product.png'
 import { formatCurrency } from 'src/utils/utils'
+import { queryClient } from 'src/main'
 
 type FormData = Pick<Schema, 'name'>
 
@@ -37,6 +38,8 @@ export default function Header() {
     onSuccess: () => {
       setIsAuthenticated(false)
       setProfile(null)
+      //Khi logout thi remove purchase
+      queryClient.removeQueries({ queryKey: ['purchases', { status: purchasesStatus.inCart }] })
     }
   })
   // Khi chung ta chuyen trang thi Header chi bi Re render
@@ -45,7 +48,9 @@ export default function Header() {
   //Nen cac query nay se khong bi inactive=> khong van thiet phai set Stale: Infinity
   const { data: purchasesInCartData } = useQuery({
     queryKey: ['purchases', { status: purchasesStatus.inCart }],
-    queryFn: () => purchaseApi.getPurchases({ status: purchasesStatus.inCart })
+    queryFn: () => purchaseApi.getPurchases({ status: purchasesStatus.inCart }),
+    //Khong Call Api purchase khi Logout
+    enabled: isAuthenticated
   })
   console.log('dataaaaaaaaaaaa', purchasesInCartData)
 
@@ -262,9 +267,11 @@ export default function Header() {
                     d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
                   />
                 </svg>
-                <span className='absolute left-[17px] top-[-5px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange'>
-                  {purchasesIncart?.length}
-                </span>
+                {purchasesInCartData && (
+                  <span className='absolute left-[17px] top-[-5px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange'>
+                    {purchasesIncart?.length}
+                  </span>
+                )}
               </Link>
             </Popover>
           </div>
